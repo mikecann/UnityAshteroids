bfe099114d99ac4a04dd3d2f97f81f377e294a98
Jos Yule <jos@theorganization.net>
2014-07-08 22:30:45 -0400

    Extension Method to support pre v4.5 Unity
    Added ‘AddRelativeForce’ method to Rigidbody2D, to replicate the
    functionality in > v4.3

---
+++ Assets/Scripts/Utilities/ExtensionMethods.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class ExtensionMethods 
+{
+#if UNITY_4_3
+	public static void AddRelativeForce(this Rigidbody2D body, Vector2 force)
+	{
+		body.AddForce(body.transform.TransformDirection(force));
+	}
+#endif
+}
